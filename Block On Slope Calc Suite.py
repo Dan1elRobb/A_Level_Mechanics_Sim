ecfd6db41ffc88f59b9235a99ea28ac8d8177fc0
danrob0910 <danrob0910@highgateschool.org.uk>
2023-09-13 13:01:09 +0100

    finished calcs

--- Block On Slope Calc Suite.py
+++ Block On Slope Calc Suite.py
@@ -5,38 +5,62 @@ G = 9.8
 
 
 def calc_a(m, angle, mew):
-    r = m * G * math.cos(angle)
+    r = m * G * math.cos(math.radians(angle))
     f = mew * r
-    a = (m * G * math.sin(angle) - f) / m
+    a = (m * G * math.sin(math.radians(angle)) - f) / m
     return a
 
 
 def calc_m(f, angle, mew):
-    m = f / (mew * G * math.cos(angle))
+    m = f / (mew * G * math.cos(math.radians(angle)))
     return m
 
 
 def calc_f_m(mew, m, angle):
-    f = mew * m * G * math.cos(angle)
+    f = mew * m * G * math.cos(math.radians(angle))
     return f
 
 
 def calc_angle(f, mew, m):
     angle = math.acos(f / (mew * m * G))
-    return angle
+    return math.degrees(angle)
 
 
 def calc_m_with_acc(angle, a, f):
-    m = f / ((G * math.sin(angle)) - a)
+    m = f / ((G * math.sin(math.radians(angle))) - a)
     return m
 
 
-m = int(input('Mass: '))
+'''m = int(input('Mass: '))
 angle = int(input('Angle: '))
 f = int(input('Friction: '))
-mew = int(input('Mew: '))
-a = int(input('Acceleration: '))
+mew = float(input('Mew: '))
+a = int(input('Acceleration: ')'''
+
+message = 'Enter the question variables'
+box_title = 'Blocks on slopes'
+var_names = ['Mass', 'Angle', 'Friction', 'Coefficient of Friction', 'Acceleration']
+var_values = []
+var_values = easygui.multenterbox(message, box_title, var_names)
+
+m = int(var_values[0])
+angle = int(var_values[1])
+f = int(var_values[2])
+mew = int(var_values[3])
+a = int(var_values[4])
 
 if m == -1 and a != -1 and angle != -1 and f != -1:
-    m = calc_m_with_acc(angle,a,f)
+    m = calc_m_with_acc(angle, a, f)
     print(m)
+if m == -1 and mew != -1 and angle != -1 and f != -1:
+    m = calc_m(f, angle, mew)
+    print(m)
+if a == -1 and angle != -1 and mew != -1 and m != -1:
+    a = calc_a(m, angle, mew)
+    print(a)
+if f == -1 and mew != -1 and m != -1 and angle != -1:
+    f = calc_f_m(mew, m, angle)
+    print(f)
+if angle == -1 and f != -1 and mew != -1 and m != -1:
+    angle = calc_angle(f, mew, m)
+    print(angle)
