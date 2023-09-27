55219c5a8b3b63195a1e5b1db5473032d97b32e9
danrob0910 <danrob0910@highgateschool.org.uk>
2023-09-27 12:58:28 +0100

    slopes  with pymunk sorted (ish)

--- blocks_on_slopes_simulation.py
+++ blocks_on_slopes_simulation.py
@@ -1,46 +1,51 @@
-import pygame
-from pygame.locals import *
-import math
-pygame.init()
-window = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
-FPS = 60
-x_pos = 0
-time = 20
-slope_x = 0
-slope_y = 600
-rotation = 0
-rotation_speed = 2
-WHITE = (0,0,0)
+import pymunk
+import pymunk.pygame_util
+import pygame as pg
+
+
+# Initialise pygame
+pg.init()
+# Define window size as a constant and at create the pygame window
+WINDOW_SIZE = 1280, 720
+window = pg.display.set_mode(WINDOW_SIZE)
+
+# Define colours
 BLACK = (255,255,255)
-image_orig = pygame.Surface((3000,10))
-image_orig.set_colorkey(WHITE)
-image_orig.fill(BLACK)
-image = image_orig.copy()
-image.set_colorkey(WHITE)
-slope = image.get_rect()
-slope.centre = (1280//2, 720//2)
+# Allow Pymunk to draw to the pygame window
+draw_options = pymunk.pygame_util.DrawOptions(window)
+
+# Create the pymunk space and assign it a gravity vector
+space = pymunk.Space()
+space.gravity = 0, -900
+
+# Create slope
+slope_static = space.static_body
+slope_segment = pymunk.Segment(slope_static, (0,0), (WINDOW_SIZE[0],0), 4)
+slope_segment.elasticity = 1
+
+# Create Block
+block_body = pymunk.Body(mass=1, moment=10)
+block_body.position = 100, 200
+block = pymunk.Circle(block_body, radius=20)
+block.elasticity = 0.9
+
+# Add Block to space
+space.add(block_body, block, slope_segment)
+
+# Pygame event loop
+
+running = True
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == K_RIGHT:
-                x_pos += 1280/(time*2)
-            if event.key == K_LEFT:
-                x_pos -= 1280/(time*2)
-    old_centre = slope.center
-    rotation = (rotation + rotation_speed) % 360
-    new_image = pygame.transform.rotate(image_orig, rotation)
-    slope = new_image.get_rect()
-    slope.centre = old_centre
-    window.fill('white')
-    window.blit(new_image , slope)
-    '''pygame.draw.rect(window,'black',pygame.Rect(0,675,1280,5))
-    pygame.draw.rect(window, 'black', pygame.Rect(x_pos, 680, 40, 40)
-    pygame.display.update()
-    print(x_pos)'''
-    pygame.display.flip()
-    clock.tick(FPS)
-pygame.quit()
+    window.fill(BLACK)
+    space.debug_draw(draw_options)
+    pg.display.update()
+    space.step(0.01)
+
+pg.quit()
+
+
+
+
