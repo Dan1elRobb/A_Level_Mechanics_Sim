0cb52e6c41440748ce0a1737288e89920f3a5ce5
danrob0910 <danrob0910@highgateschool.org.uk>
2023-09-29 10:31:52 +0100

    added slope stupid angles

--- blocks_on_slopes_simulation.py
+++ blocks_on_slopes_simulation.py
@@ -1,51 +1,62 @@
 import pymunk
 import pymunk.pygame_util
 import pygame as pg
-
-
+import math
 # Initialise pygame
 pg.init()
 # Define window size as a constant and at create the pygame window
-WINDOW_SIZE = 1280, 720
+WINDOW_SIZE = (1280, 720)
 window = pg.display.set_mode(WINDOW_SIZE)
+ANGLE = 30
+print(math.radians(ANGLE))
 
 # Define colours
-BLACK = (255,255,255)
+BLACK = (255, 255, 255)
 # Allow Pymunk to draw to the pygame window
 draw_options = pymunk.pygame_util.DrawOptions(window)
 
 # Create the pymunk space and assign it a gravity vector
 space = pymunk.Space()
-space.gravity = 0, -900
+space.gravity = 0, 100
 
 # Create slope
+SLOPE_Y_POS_BECAUSE_OF_ANGLE = 1280 * math.tan(math.radians((ANGLE)))
 slope_static = space.static_body
-slope_segment = pymunk.Segment(slope_static, (0,0), (WINDOW_SIZE[0],0), 4)
+slope_segment = pymunk.Segment(slope_static, (0, 720), (1280, SLOPE_Y_POS_BECAUSE_OF_ANGLE), 4)
 slope_segment.elasticity = 1
 
+
 # Create Block
-block_body = pymunk.Body(mass=1, moment=10)
-block_body.position = 100, 200
-block = pymunk.Circle(block_body, radius=20)
-block.elasticity = 0.9
+block_body = pymunk.Body(mass=5, moment=10)
+block_body.position = 1000, 20
+block = pymunk.Poly.create_box(block_body, (50, 50))
+
 
 # Add Block to space
 space.add(block_body, block, slope_segment)
 
+
 # Pygame event loop
 
-running = True
-while running:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
-    window.fill(BLACK)
-    space.debug_draw(draw_options)
-    pg.display.update()
-    space.step(0.01)
+class Sim:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode(WINDOW_SIZE)
+        self.draw_options = pymunk.pygame_util.DrawOptions(self.screen)
+        self.running = True
 
-pg.quit()
+    def run(self):
+        while self.running:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.running = False
+
+            self.screen.fill((220, 220, 220))
+            space.debug_draw(self.draw_options)
+            pg.display.update()
+            space.step(0.01)
+
+        pg.quit()
 
 
-
-
+Sim().run()
