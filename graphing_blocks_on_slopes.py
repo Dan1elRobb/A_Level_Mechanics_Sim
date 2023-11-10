0f8e04cfb13f0ff58dea8bfbdf6c5c5adfa5cc80
danrob0910 <danrob0910@highgateschool.org.uk>
2023-11-10 10:28:49 +0000

    slope :D

--- graphing_blocks_on_slopes.py
+++ graphing_blocks_on_slopes.py
@@ -1,25 +1,38 @@
-import math
 import matplotlib.pyplot as plt
 import numpy as np
-def calc_v_over_time(angle,mew,end_time):
-    t = 0
-    time_list = []
-    vel_list = []
-    while t < end_time:
-        for i in range((end_time*100)+1):
-            v = 9.8*t*(math.sin(math.radians(angle)) - math.cos(math.radians(angle))*mew)
-            vel_list.append(v)
-            time_list.append(t)
-            t += 0.01
-    return time_list,vel_list
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from BlockOnSlopeCalcSuite import calc_v_over_time
 
-print(max(calc_v_over_time(60,0.5,10)[1]))
 
-fig = plt.figure(figsize=(20,30))
+class VelocityTimeGraphFrame(tk.Frame):
+    def __init__(self):
+        super().__init__()
+        fig = plt.figure(figsize=(20, 30))
+        ax = plt.axes()
+        ax.plot(calc_v_over_time(60, 0.5, 10)[0], calc_v_over_time(60, 0.5, 10)[1])
+        plt.xticks(np.arange(0, max(calc_v_over_time(60, 0.5, 10)[0]) + 0.5, 0.5))
+        plt.yticks(np.arange(0, max(calc_v_over_time(60, 0.5, 10)[1]) + 0.5, 0.5))
+        plt.xlim(0, 10)
+        plt.ylim(0, max(calc_v_over_time(60, 0.5, 10)[1]))
+
+        canvas = FigureCanvasTkAgg(fig, master=self)
+        canvas.draw()
+
+        canvas.get_tk_widget().pack()
+
+
+root = tk.Tk()
+root.title('Velocity time graph')
+graph_page = VelocityTimeGraphFrame()
+graph_page.pack()
+root.mainloop()
+
+'''fig = plt.figure(figsize=(20,30))
 ax = plt.axes()
 ax.plot(calc_v_over_time(60,0.5,10)[0],calc_v_over_time(60,0.5,10)[1])
 plt.xticks(np.arange(0,max(calc_v_over_time(60,0.5,10)[0])+0.5,0.5))
 plt.yticks(np.arange(0,max(calc_v_over_time(60,0.5,10)[1])+0.5,0.5))
 plt.xlim(0,10)
 plt.ylim(0,max(calc_v_over_time(60,0.5,10)[1]))
-plt.show()
+plt.show()'''
