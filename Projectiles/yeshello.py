1345121d9423b2e5762665b3e73955f108bd6141
dan1elrobb <danrob0910@highgateschool.org.uk>
2024-01-16 21:26:14 +0000

    i MIGHT be the goat

---
+++ Projectiles/yeshello.py
@@ -0,0 +1,37 @@
+from yeahyeqah import run_sim
+
+import tkinter as tk
+from tkinter import messagebox
+
+
+class SimulationApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Projectile Simulation")
+        self.master.geometry("300x150")
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        # Button to start the simulation
+        start_button = tk.Button(self.master, text="Start Sim", command=self.start_simulation)
+        start_button.pack(pady=20)
+
+        # Button to exit the application
+        exit_button = tk.Button(self.master, text="Exit", command=self.exit_application)
+        exit_button.pack()
+
+    def start_simulation(self):
+        # Add your simulation start functionality here
+        run_sim()
+
+    def exit_application(self):
+        result = messagebox.askquestion("Exit", "Are you sure you want to exit?", icon='warning')
+        if result == 'yes':
+            self.master.destroy()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = SimulationApp(root)
+    root.mainloop()
